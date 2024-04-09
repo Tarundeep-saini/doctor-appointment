7985e6712b45153986b2a62ad22f80de534bd472
Tarundeep <105970598+Tarundeep-saini@users.noreply.github.com>
2024-04-09 16:53:42 +0530

    Update page.jsx

--- app/(route)/my-booking/page.jsx
+++ app/(route)/my-booking/page.jsx
@@ -1,4 +1,4 @@
-"use client ";
+"use client";
 import React, { useEffect, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
